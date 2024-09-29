64c156fb00a36192ee5f1a0f5f8447321683385a
hairo88 <inouekotarou73@gmail.com>
2024-09-30 00:01:17 +0900

    一旦完成'

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,6 +1,41 @@
 #include "ex01.h"
 
-int main(int ac, char *av[])
+int main(void)
 {
-	
+	PhoneBook phoneBook;
+	std::string command;
+
+	while (true) {
+		command = getInput("Enter command (ADD, SEARCH, EXIT): ");
+
+		for (size_t i = 0; i < command.length(); i++)
+			command[i] = std::toupper(command[i]);
+
+		if (command == "ADD") {
+			Contact newContact;
+			newContact.setContact(
+				getInput("Enter first name: "),
+				getInput("Enter last name: "),
+				getInput("Enter nickname: "),
+				getInput("Enter phone number: "),
+				getInput("Enter darkest secret: ")
+			);
+			phoneBook.addContact(newContact);
+		}
+		else if (command == "SEARCH") {
+			phoneBook.searchContacts();
+			int index;
+			std::cout << "Enter index to display: ";
+			std::cin >> index;
+			std::cin.ignore();
+			phoneBook.displayContact(index);
+		}
+		else if (command == "EXIT")
+			break;
+		else
+			std::cout << "Invalid command" << std::endl;
+	}
+
+	std::cout << "Boodbye! All contacts are lost forever!" << std::endl;
+	return 0;
 }
